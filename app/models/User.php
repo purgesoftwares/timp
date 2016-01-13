45b5d189777e559891d5b77de94909133ee0c57c
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2016-01-14 00:47:01 +0530

    updated uesers model

--- app/models/User.php
+++ app/models/User.php
@@ -3,70 +3,71 @@
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
+class User extends Eloquent implements UserInterface/* , RemindableInterface {
+ */
+ {
 	/**
 	 * The database table used by the model.
 	 *
 	 * @var string
 	 */
-	protected $table = 'users';
+	protected $table = 'fbusers';
 
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
 	 * @var array
-	 */
-	protected $hidden = array('password');
+	 // */
+	// protected $hidden = array('password');
 
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
+	// /**
+	 // * Get the unique identifier for the user.
+	 // *
+	 // * @return mixed
+	 // */
+	// public function getAuthIdentifier()
+	// {
+		// return $this->getKey();
+	// }
 
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
+	// /**
+	 // * Get the password for the user.
+	 // *
+	 // * @return string
+	 // */
+	// public function getAuthPassword()
+	// {
+		// return $this->password;
+	// }
 
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+	// /**
+	 // * Get the e-mail address where password reminders are sent.
+	 // *
+	 // * @return string
+	 // */
+	// public function getReminderEmail()
+	// {
+		// return $this->email;
+	// }
 
-	public function getRememberToken()
-	{
-	    return $this->remember_token;
-	}
+	// public function getRememberToken()
+	// {
+	    // return $this->remember_token;
+	// }
 
-	public function setRememberToken($value)
-	{
-	    $this->remember_token = $value;
-	}
+	// public function setRememberToken($value)
+	// {
+	    // $this->remember_token = $value;
+	// }
 
-	public function getRememberTokenName()
-	{
-	    return 'remember_token';
-	}
+	// public function getRememberTokenName()
+	// {
+	    // return 'remember_token';
+	// }
 
-	public function profiles()
-	{
-		return $this->hasMany('Profile');
-	}
+	// public function profiles()
+	// {
+		// return $this->hasMany('Profile');
+	// }
 
 }
