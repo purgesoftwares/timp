27c08611755e276333eaa1f3f6ae89b30dbdaeaa
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2016-01-17 22:37:59 +0530

    Removing message

--- app/routes.php
+++ app/routes.php
@@ -287,11 +287,11 @@ Route::get('login/fb/callback', function() {
    
 
         // $image['access_token']  = $_SESSION['token'];
-        // $image['message']       = '';
-		$image['message']       = 'Change your profile picture for a Tribute to Indian Martyrs - Phathankot :: '.$me['name'].'
-                                            Create It Now, Click here http://tribute.purgesoft.com/login/fb
-                                            Show country love on this #RepublicDay' ;
-                //$image['message']       = 'Change your profile picture for a Tribute to Indian Martyrs - Phathankot :: '.$me['name'].'
+        $image['message']       = '';
+		// $image['message']       = 'Change your profile picture for a Tribute to Indian Martyrs - Phathankot :: '.$me['name'].'
+                                            // Create It Now, Click here http://tribute.purgesoft.com/login/fb
+                                            // Show country love on this #RepublicDay' ;
+                // $image['message']       = 'Change your profile picture for a Tribute to Indian Martyrs - Phathankot :: '.$me['name'].'
                 //$image['message']       = 'Change your profile picture for a Tribute to Indian Martyrs - Phathankot :: '.$me['name'].'
 											//Create It Now, Click here http://tribute.purgesoft.com/login/fb' ;
                 //$image['image']         = '@'.realpath(public_path().'/avatar/new-profile-'.$uid.'.jpg');
